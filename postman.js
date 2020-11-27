52f8f2b5d1149e6f9a265a803e5709bb2830dcbf
Fatai Balogun <fattylee.remod@gmail.com>
2020-11-27 22:07:30 +0100

    add postman using axios - for route testing

---
+++ postman.js
@@ -0,0 +1,18 @@
+const _axios = require("axios");
+
+const axios = _axios.create({ baseURL: "http://localhost:5000/api/v1" });
+
+(async () => {
+  try {
+    const { data } = await axios.post("/auth/login", {
+      username: "fattyleezs",
+
+      password: "password",
+      email: "f63atai4humility@yahoo.com",
+    });
+
+    console.log(data);
+  } catch (ex) {
+    console.log(ex.response);
+  }
+})();
